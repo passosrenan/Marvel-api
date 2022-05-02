48bea0f1554b5b4bd396d99586a96d1179867832
Renan <passosrenan325@gmail.com>
2022-05-02 17:28:24 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios'
+import md5 from 'md5'
 
-function App() {
+const baseUrl = 'http://gateway.marvel.com/v1/public/characters?'
+const publicKey = '56263709d5af23f6cbb1a43f7c9d2838'
+const privateKey = '1dbcef0df0fac6146a21f9a70ed49342cca13660'
+
+const time = Number(new Date())
+
+const hash = md5(time + privateKey + publicKey)
+
+const App: React.FC = () => {
+
+  useEffect(() => {
+    axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`).then(response => console.log(response)).catch(erro => console.log(erro))
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Marvel API
     </div>
   );
 }
